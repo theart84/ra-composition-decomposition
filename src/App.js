333c4a899fec887a0254d8aff5a2703de1041ecc
theart84 <artshadow74@gmail.com>
2021-07-17 13:55:41 +0500

    ads banner added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Currency from "./components/Currency/Currency";
 import Search from "./components/Search/Search";
 import WidgetContainer from "./components/WidgetContainer/WidgetContainer";
 import Service from "./components/Service/Service";
+import AdsBanner from "./components/AdsBanner/AdsBanner";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Currency/>
       <Service/>
       <Search/>
+      <AdsBanner href="/" imageURL="https://i.imgur.com/pArPD6N.png" imageClasses="image" bannerClasses="banner-container"/>
       <WidgetContainer/>
     </div>
   );
